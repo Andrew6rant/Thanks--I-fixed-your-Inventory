eca20d435c33213882fe1c9e4b3d6d8c4f48d309
Andrew Grant <andrew.grant@mail.rit.edu>
2023-08-15 23:23:08 -0400

    implement more customization features
    recipe book x and y, crafting text x and y, player render size

--- src/client/java/io/github/andrew6rant/tifyi/mixin/client/InventoryScreenMixin.java
+++ src/client/java/io/github/andrew6rant/tifyi/mixin/client/InventoryScreenMixin.java
@@ -4,21 +4,41 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.screen.ingame.AbstractInventoryScreen;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+import net.minecraft.client.gui.screen.recipebook.RecipeBookWidget;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static io.github.andrew6rant.tifyi.config.Config.playerRenderX;
-import static io.github.andrew6rant.tifyi.config.Config.playerRenderY;
+import static io.github.andrew6rant.tifyi.config.Config.*;
 
 @Environment(EnvType.CLIENT)
 @Mixin(InventoryScreen.class)
 public abstract class InventoryScreenMixin extends AbstractInventoryScreen<PlayerScreenHandler> {
+    @Shadow @Final private RecipeBookWidget recipeBook;
+
+    @Shadow private boolean mouseDown;
+
     public InventoryScreenMixin(PlayerScreenHandler screenHandler, PlayerInventory playerInventory, Text text) {
         super(screenHandler, playerInventory, text);
+    }
+
+    @Inject(method = "<init>", at = @At(value = "TAIL"))
+    public void tifyi$moveCraftingTextXYOnInit(PlayerEntity player, CallbackInfo ci) {
+        this.titleX = craftingTextX;
+        this.titleY = craftingTextY;
+    }
+
+    @ModifyConstant(method = "drawBackground(Lnet/minecraft/client/gui/DrawContext;FII)V",
+            constant = @Constant(intValue = 30, ordinal = 0))
+    public int tifyi$changePlayerRenderSize(int constant) {
+        return playerRenderSize;
     }
 
     @ModifyConstant(method = "drawBackground(Lnet/minecraft/client/gui/DrawContext;FII)V",
@@ -31,6 +51,26 @@ public abstract class InventoryScreenMixin extends AbstractInventoryScreen<Playe
             constant = @Constant(intValue = 75, ordinal = 0))
     public int tifyi$movePlayerRenderY(int constant) {
         return playerRenderY;
+    }
+
+    @ModifyArg(method = "init()V", index = 8, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/TexturedButtonWidget;<init>(IIIIIIILnet/minecraft/util/Identifier;Lnet/minecraft/client/gui/widget/ButtonWidget$PressAction;)V"))
+    private ButtonWidget.PressAction tifyi$modifyRecipeBookXY(ButtonWidget.PressAction action) {
+        return (buttonWidget) -> {
+            this.recipeBook.toggleOpen();
+            this.x = this.recipeBook.findLeftEdge(this.width, this.backgroundWidth);
+            buttonWidget.setPosition(this.x + recipeBookX, this.height / 2 - recipeBookY);
+            this.mouseDown = true;
+        };
+    }
+
+    @ModifyConstant(method = "init()V", constant = @Constant(intValue = 104))
+    private int tifyi$moveRecipeBookX(int constant) {
+        return recipeBookX;
+    }
+
+    @ModifyConstant(method = "init()V", constant = @Constant(intValue = 22, ordinal = 0))
+    private int tifyi$moveRecipeBookY(int constant) {
+        return recipeBookY;
     }
 
 }
--- src/main/java/io/github/andrew6rant/tifyi/config/Config.java
+++ src/main/java/io/github/andrew6rant/tifyi/config/Config.java
@@ -6,8 +6,18 @@ public class Config extends MidnightConfig {
     @Comment(centered = true) public static Comment immediate;
     @Entry(min = -1000, max = 1000) public static int playerRenderX = 52;
     @Entry(min = -1000, max = 1000) public static int playerRenderY = 72;
+    @Entry(min = -1000, max = 1000) public static int playerRenderSize = 30;
+
+    @Entry(min = -1000, max = 1000) public static int craftingTextX = 97;
+    @Entry(min = -1000, max = 1000) public static int craftingTextY = 6;
+
+    @Entry(min = -1000, max = 1000) public static int recipeBookX = 104;
+    @Entry(min = -1000, max = 1000) public static int recipeBookY = 22;
+
+
     @Entry(width = 9) public static String slotHighlightColorAARRGGBB = "#80FFFFFF";
     @Comment(centered = true) public static Comment reloadWorld;
+    @Comment(centered = true) public static Comment reloadExplanation;
     @Entry(min = -1000, max = 1000) public static int offhandSlotX = 80;
     @Entry(min = -1000, max = 1000) public static int offhandSlotY = 62;
     @Entry(min = -1000, max = 1000) public static int craftingResultSlotX = 152;
